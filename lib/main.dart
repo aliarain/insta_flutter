9a57b2a34bf142515c01da33c1a366c33f15b0a2
Ali Arain <deistali2@gmail.com>
2022-08-06 02:10:13 +0500

    Package Updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:insta_flutter/responsive/mobile_screen_layout.dart';
 import 'package:insta_flutter/responsive/responsive_screen_layout.dart';
 import 'package:insta_flutter/responsive/web_screen_layout.dart';
+import 'package:insta_flutter/screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,21 +19,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Instagram Flutter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const ResponsiveScreenLayout(
-          webScreenLayout: WebScreenLayout(),
-          mobileScreenLayout: MobileScreenLayout()),
+      theme: ThemeData.dark(),
+      home: const LoginScreen(),
+      //const ResponsiveScreenLayout(
+      //     webScreenLayout: WebScreenLayout(),
+      //     mobileScreenLayout: MobileScreenLayout()),
     );
   }
 }
